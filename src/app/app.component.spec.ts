1facc5fadd578ef1e52b4f3d3f4b308cdd860bad
Muhammad Naveed <naveed4034@gmail.com>
2021-10-17 04:38:06 +0500

    todo project setup initial

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -26,6 +26,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('to-do-app-client app is running!');
+    expect(compiled.querySelector('span').textContent).toContain('to-do-app-client app is running!');
   });
 });
