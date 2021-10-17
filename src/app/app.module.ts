709139d8792dc1f143df5c6f0ff289ebbf38a93c
Muhammad Naveed <naveed4034@gmail.com>
2021-10-17 05:14:02 +0500

    bootstrap for angular package added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { TodoComponent } from './todo/todo.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app.routing.module';
 @NgModule({
